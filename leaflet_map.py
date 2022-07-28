bbdfb56ad043c0c33dae9ac040a508d74aa48056
pperezdev <pierrick.perez@free.fr>
2022-07-28 14:38:14 +0200

    modifu

--- leaflet_map.py
+++ leaflet_map.py
@@ -1,7 +1,21 @@
 import geopandas
+import matplotlib.pyplot as plt
+import pandas as pd
 
-filename = "data/Planning_Districts.geojson"
-file = open(filename)
-df = geopandas.read_file(file)
-df.head()
-df.plot()
+filename_map = "data/Planning_Districts.geojson"
+filename_crimes = "data/crime_boston.csv"
+
+file_map = open(filename_map)
+df_crimes_raw = pd.read_csv(filename_crimes, encoding='utf8', sep=';')
+
+df_crimes_raw = df_crimes_raw.drop(df_crimes_raw[df_crimes_raw['Lat'] != 0])
+
+df_map = geopandas.read_file(file_map)
+df_crimes = geopandas.GeoDataFrame(df_crimes_raw,
+                             geometry=geopandas.GeoSeries.from_xy(df_crimes_raw['Long'], df_crimes_raw['Lat']),
+                             crs=4326)
+
+print(df_map.head())
+df_map.plot(column='PLANNING_D')
+df_crimes.plot(marker='*', color='green', markersize=5);
+plt.show()
